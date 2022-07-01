1699c631c3845e45b7508064469592f1b411f1ee
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-07-02 02:59:33 +0700

    Added styling and ID

--- src/components/GremlinComponent.js
+++ src/components/GremlinComponent.js
@@ -24,24 +24,26 @@ class GremlinComponent extends React.Component{
     render(){
         return(
             <div>
-                <h1 className="text-center">Gremlins List</h1>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
+                <h1 className="text-center bg-primary text-warning">Gremlins List</h1>
+                <table className="table table-striped ">
+                    <thead >
+                        <tr className="table-row">
+                        <th >ID</th>
                             <th>Name</th>
                             <th>Age</th>
                             <th>Color</th>
-                            <th>Is Evil?</th>
+                            <th className="table-column">Is Evil?</th>
                         </tr>
                     </thead>
 
                     <tbody>
                         {this.state.gremlins.map(gremlin => (
-                            <tr key={gremlin.id}>
-                                <td>{gremlin.name}</td> 
-                                <td>{gremlin.age}</td>
-                                <td>{gremlin.color}</td>
-                                <td>{gremlin.isEvil ? "Yes" : "No"}</td>
+                            <tr key={gremlin.id} className="rows">
+                            <td className="rows">{gremlin.id}</td>
+                                <td className="rows">{gremlin.name}</td> 
+                                <td className="rows">{gremlin.age}</td>
+                                <td className="rows">{gremlin.color}</td>
+                                <td className="rows">{gremlin.isEvil ? "Yes" : "No"}</td>
                             </tr>
                         ))}
                     </tbody>
